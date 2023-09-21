a35e813296c5913b83e3ab55a9ff2b7abc36ec38
AWAIKEN1 <alistairaiken@gmail.com>
2023-09-21 15:25:22 +0100

    Python Katas update

--- tests/test_smash_words.py
+++ tests/test_smash_words.py
@@ -1,6 +1,6 @@
 from src.smash_words import smash
 
-def test_smash_words_basic():
+def test_smash_words_basic_test():
     word_array = ['hello', 'world', 'this', 'is', 'great']
     result = smash(word_array)
     assert result == 'hello world this is great'
