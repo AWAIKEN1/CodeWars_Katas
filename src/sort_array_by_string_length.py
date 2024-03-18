088ea89cd2d2b9dae0661e9b951a848f6a4a14bb
AWAIKEN1 <alistairaiken@gmail.com>
2024-03-18 21:29:35 +0000

    Python Katas refactored sort_array_by_string_length

--- src/sort_array_by_string_length.py
+++ src/sort_array_by_string_length.py
@@ -1,9 +1,14 @@
 def sort_by_length(arr):
-    n = len(arr)
+    return sorted(arr, key=len)
 
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if len(arr[j]) > len(arr[j + 1]):
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-    return arr
+
+
+#   n = len(arr)
+
+#   for i in range(n):
+#       for j in range(0, n - i - 1):
+#           if len(arr[j]) > len(arr[j + 1]):
+#               arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+#   return arr
